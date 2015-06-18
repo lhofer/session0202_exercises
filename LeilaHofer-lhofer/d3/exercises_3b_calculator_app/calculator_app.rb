2816d156ea7fa26254f09d2e9e2db69f530534bd
Leila Hofer <lkhofer@gmail.com>
2015-06-18 10:41:53 +0200

    small fixes in both 3b apps

--- LeilaHofer-lhofer/d3/exercises_3b_calculator_app/calculator_app.rb
+++ LeilaHofer-lhofer/d3/exercises_3b_calculator_app/calculator_app.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 
-get "/" do |num_one, num_two|
-	"To use this calculator type the following: 'localhost:9393/function/number1/number2' where 'function' can be either 'add', 'subtract', 'multiply', or 'divide', and 'number1'/'number2' can be any two nubmers on which you would like to perform that function."
+get "/" do
+	"To use this calculator type the following: 'localhost:9393/function/number1/number2' where 'function' can be either 'add', 'subtract', 'multiply', 'divide', or 'exponent' and 'number1'/'number2' can be any two nubmers on which you would like to perform that function."
 end
 
 get "/add/:num_one/:num_two" do |num_one, num_two|
@@ -12,10 +12,14 @@ get "/subtract/:num_one/:num_two" do |num_one, num_two|
 	"The remainder of #{num_one} minus #{num_two} is: " + (num_one.to_f - num_two.to_f).to_s
 end
 
-get "/product/:num_one/:num_two" do |num_one, num_two|
+get "/multiply/:num_one/:num_two" do |num_one, num_two|
 	"The product of #{num_one} and #{num_two} is: " + (num_one.to_f * num_two.to_f).to_s
 end
 
-get "/quotient/:num_one/:num_two" do |num_one, num_two|
+get "/divide/:num_one/:num_two" do |num_one, num_two|
 	"The quotient of #{num_one} and #{num_two} is: " + (num_one.to_f / num_two.to_f).to_s
+end
+
+get "/exponent/:num_one/:num_two" do |num_one, num_two|
+	"#{num_one} to the power of #{num_two} is: " + (num_one.to_f ** num_two.to_f).to_s
 end
