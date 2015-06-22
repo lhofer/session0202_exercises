4d67a0e1919271a86185ed7089640f23827e5952
Leila Hofer <lkhofer@gmail.com>
2015-06-23 00:08:10 +0200

    finished lab

--- LeilaHofer-lhofer/d6/Lab_6b/blog/app/controllers/articles_controller.rb
+++ LeilaHofer-lhofer/d6/Lab_6b/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
@@ -11,6 +14,10 @@ class ArticlesController < ApplicationController
   		@article = Article.new
 	end
  
+ 	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
   		@article = Article.new(article_params)
  
@@ -21,23 +28,26 @@ class ArticlesController < ApplicationController
   		end
 	end
 
-	def edit
+
+	def update
 		@article = Article.find(params[:id])
-	end
-end
 
-def update
-	@article = Article.find(params[:id])
-
-	if @article.update(article_params)
-		redirect_to @article
-	else
-		render 'edit'
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
-end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
  
-private
-	def article_params
-    	params.require(:article).permit(:title, :text)
-  	end
-
+	private
+		def article_params
+	    	params.require(:article).permit(:title, :text)
+	  	end
+end
