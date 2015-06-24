1a0a81949ae2637e7863072eb9f986edc8a43794
Leila Hofer <lkhofer@gmail.com>
2015-06-24 09:28:34 +0200

    fixed inexplicapble errors

--- LeilaHofer-lhofer/d7/outings/config/routes.rb
+++ LeilaHofer-lhofer/d7/outings/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index', to: 'welcome#index'
-  get 'welcome/contact_us', to: 'welcome#contact_us'
-  get 'welcome/:day', to: 'welcome#day'
+  get '/welcome/contact_us', to: 'welcome#contact_us'
+  get '/welcome/day', to: 'welcome#day'
 
   root 'welcome#index'
 
