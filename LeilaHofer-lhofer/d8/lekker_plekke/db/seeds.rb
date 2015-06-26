06aae37757c3f115ef83e0fcacba1c5317c111da
Leila Hofer <lkhofer@gmail.com>
2015-06-26 15:43:45 +0200

    horizontal bootstrap form

--- LeilaHofer-lhofer/d8/lekker_plekke/db/seeds.rb
+++ LeilaHofer-lhofer/d8/lekker_plekke/db/seeds.rb
@@ -2,6 +2,8 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
+Place.delete_all
+
 Place.create!({name: "Rick's", rating: 10, info: "Love.", location: "Cape Town"})
 Place.create!({name: "Sababa", rating: 10, info: "Everyone goes here for lunch. Everyone.", location: "Cape Town"})
 Place.create!({name: "The Table", rating: 10, info: "It's really tall.", location: "Cape Town"})
